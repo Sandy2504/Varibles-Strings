d3f195cfcbb72aa5bda2ca8b73ba74d798caf626
Sandra Davies <dav.sandra1970@gmail.com>
2024-03-20 12:05:38 +0100

    update

--- script.js
+++ script.js
@@ -12,12 +12,20 @@ console.log(getUserNameLength(userName) > 4);
 
 // TODO: Implement the isString function
 
-const isString = function (myData) {
-  return typeof myData === "string";
-};
-
+function isString(data) {
+  return typeof data === "string";
+}
 console.log(isString("Hello"));
+// result should be true
+
 console.log(isString(3));
+// result should be false
+
 console.log(isString(undefined));
+// result should be false
+
 console.log(isString(""));
+// result should be true
+
 console.log(isString("John" + "Doe"));
+// result should be true
